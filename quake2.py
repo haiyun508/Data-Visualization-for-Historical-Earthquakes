cbb162d01e62e815850684d31848a4fccb8f51f2
Steve Meyer <sfmeyer@gmail.com>
2021-06-12 12:04:10 -0700

    final

--- quake2.py
+++ quake2.py
@@ -8,9 +8,7 @@ from flask import render_template
 # create instance of Flask app
 app = Flask(__name__)
 
-# @app.route("/")
-
-# Read sqlite query results into a pandas DataFrame
+# Connect to postgres DB
 con = psycopg2.connect(
     host = "127.0.0.1",
     database = "earthquakes",
@@ -21,40 +19,10 @@ con = psycopg2.connect(
 
 db_cursor = con.cursor()
 
-
-# ------------------
-# method 1: query directly
-# ------------------
-
-# try:
-#     db_cursor.execute("""SELECT * from earthquake LIMIT 10""")
-# except:
-#     print("I can't SELECT from bar")
-
-# query_results = db_cursor.fetchall()
-# print(query_results)
-
-
-# ------------------
-# method 2: query and store in dataframe
-# ------------------
-
-# def create_pandas_table(sql_query, database = con):
-#     table = pd.read_sql_query(sql_query, database)
-#     return table
-
-# # Utilize the create_pandas_table function to create a Pandas data frame
-# # Store the data as a variable
-# quake_info = create_pandas_table("SELECT * from earthquake LIMIT 10")
-# print(quake_info)
-
-
-
-# WORKS!!!!!
-# create route that renders index.html template
+# create route that renders results.html template
 @app.route("/", methods=['post', 'get'])
 def index():
-    db_cursor.execute("select * from earthquake LIMIT 15")
+    db_cursor.execute("select * from earthquake LIMIT 20")
     result = db_cursor.fetchall()
     return render_template("results.html", data=result)
 
