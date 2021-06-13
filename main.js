314942495ae57bd441f29fcfcce714059baffe1f
Steve Meyer <sfmeyer@gmail.com>
2021-06-13 12:58:28 -0700

    bug fix

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 var images = {
         freq_vs_time:
-        "/tinahtmlpushto/static/img/freq_vs_time.png",
+        "/project-2/tinahtmlpushto/static/img/freq_vs_time.png",
         mag_vs_time:
-        "/tinahtmlpushto/static/img/mag_vs_time.png",
+        "/project-2/tinahtmlpushto/static/img/mag_vs_time.png",
         depth_vs_time:
-        "/tinahtmlpushto/static/img/depth_vs_time.png",
+        "/project-2/tinahtmlpushto/static/img/depth_vs_time.png",
         Mag_5p5_6_vs_time:
-        "/tinahtmlpushto/static/img/5.5-6_vs_time.png",
+        "/project-2/tinahtmlpushto/static/img/5.5-6_vs_time.png",
         Mag_6_6p5_vs_time:
-        "/tinahtmlpushto/static/img/6-6.5_vs_time.png",
+        "/project-2/tinahtmlpushto/static/img/6-6.5_vs_time.png",
         Mag_6p5_7_vs_time:
-        "/tinahtmlpushto/static/img/6.5-7_vs_time.png",
+        "/project-2/tinahtmlpushto/static/img/6.5-7_vs_time.png",
         Mag_7_8_vs_time:
-        "/tinahtmlpushto/static/img/7-8_vs_time.png",
+        "/project-2/tinahtmlpushto/static/img/7-8_vs_time.png",
         Mag_8_9p5_vs_time:
-        "/tinahtmlpushto/static/img/8-9.5_vs_time.png",
+        "/project-2/tinahtmlpushto/static/img/8-9.5_vs_time.png",
     default:
         "https://wanna-joke.com/wp-content/uploads/2014/10/funny-Arnold-Schwarzenegger-protein.jpg"
 };
